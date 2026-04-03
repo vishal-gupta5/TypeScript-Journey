f238ec93165b92bb7021de7acc12376bac3e2e0f
Vishal Gupta <guptaaryan1103@gmail.com>
2026-04-04 00:35:13 +0530

    Create a Tuple

--- Lecture2/app.ts
+++ Lecture2/app.ts
@@ -26,5 +26,19 @@ favoriteLanguage = ["Hindi", "English"];
 let bioData: any[];
 bioData = ["Vishal Gupta", 12, true]; // Fixed array of two types 0 -> numbers, 1 -> string
 
-
+// Tuple
+const human: {
+  name: string;
+  age: number;
+  skills: string[];
+  products: [number, string]
+} = {
+  name: "Aryan Gupta",
+  age: 25,
+  skills: ["Web Development", "App Development"],
+  products: [11, "Windows"]
+};
+ 
+console.log(person.xyz.address);
+console.log(person.skills);
 console.log(person.firstName);
