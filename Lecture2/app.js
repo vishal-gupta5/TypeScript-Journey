2c30f1ebcdb036e949d1d43e4216bad94e6c9638
Vishal Gupta <guptaaryan1103@gmail.com>
2026-04-04 10:36:30 +0530

    Create an Enum

--- Lecture2/app.js
+++ Lecture2/app.js
@@ -14,14 +14,36 @@ const person = {
 let favoriteLanguage;
 favoriteLanguage = ["Hindi", "English"];
 let bioData;
-bioData = ["Vishal Gupta", 12, true];
+bioData = ["Vishal Gupta", 12, true]; // Fixed array of two types 0 -> numbers, 1 -> string
 // Tuple
 const human = {
     name: "Aryan Gupta",
     age: 25,
     skills: ["Web Development", "App Development"],
-    products: [11, "Windows"]
+    products: [11, "Windows"],
 };
-console.log(person.firstName);
+// Enum
+var Status;
+(function (Status) {
+    Status[Status["Pending"] = 0] = "Pending";
+    Status[Status["Approved"] = 1] = "Approved";
+    Status[Status["Rejected"] = 2] = "Rejected";
+})(Status || (Status = {}));
+const info = {
+    name: "Harsh",
+    age: 25,
+    status: Status.Pending,
+};
+if (info.status === Status.Pending) {
+    console.log("Pending....");
+}
+else if (info.status === Status.Approved) {
+    console.log("Approved....");
+}
+else {
+    console.log("Rejected....");
+}
 console.log(person.xyz.address);
 console.log(person.skills);
+console.log(person.firstName);
+console.log(human.products);
--- Lecture2/app.ts
+++ Lecture2/app.ts
@@ -31,14 +31,37 @@ const human: {
   name: string;
   age: number;
   skills: string[];
-  products: [number, string]
+  products: [number, string];
 } = {
   name: "Aryan Gupta",
   age: 25,
   skills: ["Web Development", "App Development"],
-  products: [11, "Windows"]
+  products: [11, "Windows"],
 };
- 
+
+// Enum
+enum Status {
+  Pending,
+  Approved,
+  Rejected,
+}
+
+const info = {
+  name: "Harsh",
+  age: 25,
+  status: Status.Pending,
+};
+
+if (info.status === Status.Pending) {
+  console.log("Pending....");
+} else if (info.status === Status.Approved) {
+  console.log("Approved....");
+} else {
+  console.log("Rejected....");
+}
+
+
 console.log(person.xyz.address);
 console.log(person.skills);
 console.log(person.firstName);
+console.log(human.products);
